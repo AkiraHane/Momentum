aff693412189e0603f6a9415f8630a0ecf56f5ed
AkiraHane <akirahane@foxmail.com>
2026-05-22 15:46:59 +0800

    modify 键位修改

--- src/main/java/com/akirahane/momentum/MomentumClient.java
+++ src/main/java/com/akirahane/momentum/MomentumClient.java
@@ -2,6 +2,7 @@ package com.akirahane.momentum;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.Identifier;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
@@ -21,14 +22,24 @@ import org.lwjgl.glfw.GLFW;
 // 你可以使用 EventBusSubscriber 来自动注册类中所有带有 @SubscribeEvent 注解的静态方法
 @EventBusSubscriber(modid = Momentum.MODID, value = Dist.CLIENT)
 public class MomentumClient {
+    public static final KeyMapping.Category MOMENTUM_CATEGORY = new KeyMapping.Category(
+            Identifier.fromNamespaceAndPath(Momentum.MODID, "category")
+    );
     // 键映射是延迟初始化的，因此在注册之前它并不会存在。
-    public static final Lazy<@NotNull KeyMapping> KEY_MAPPING = Lazy.of(() -> new KeyMapping(
+    public static final Lazy<@NotNull KeyMapping> LOWER_CENTER_KEY_MAPPING = Lazy.of(() -> new KeyMapping(
             "key.momentum.lower_center", // 将使用此翻译密钥进行本地化处理
+            KeyConflictContext.IN_GAME, // 游戏中
+            InputConstants.Type.KEYSYM, // 默认键盘键位
+            GLFW.GLFW_KEY_C, // 按下C
+            KeyMapping.Category.MOVEMENT // 移动类别
+    ));
+    public static final Lazy<@NotNull KeyMapping> CHANGE_MOMENTUM_KEY_MAPPING = Lazy.of(() -> new KeyMapping(
+            "key.momentum.change_momentum", // 将使用此翻译密钥进行本地化处理
             KeyConflictContext.IN_GAME, // 游戏中
             KeyModifier.SHIFT, // 按下SHIFT
             InputConstants.Type.KEYSYM, // 默认键盘键位
             GLFW.GLFW_KEY_M, // 同时按下M
-            KeyMapping.Category.MOVEMENT // 移动类别
+            MOMENTUM_CATEGORY // 自定义类别
     ));
 
     public MomentumClient(ModContainer container) {
@@ -40,7 +51,10 @@ public class MomentumClient {
 
     @SubscribeEvent // on the mod event bus only on the physical client
     public static void registerBindings(RegisterKeyMappingsEvent event) {
+        // 注册类别
+        event.registerCategory(MOMENTUM_CATEGORY);
         // 注册按键绑定
-        event.register(KEY_MAPPING.get());
+        event.register(LOWER_CENTER_KEY_MAPPING.get());
+        event.register(CHANGE_MOMENTUM_KEY_MAPPING.get());
     }
 }
