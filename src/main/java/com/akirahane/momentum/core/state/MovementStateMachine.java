1ac9c16251fca599535a25320e49880430fad57f
AkiraHane <akirahane@foxmail.com>
2026-06-28 17:38:25 +0800

    modify 去除debug日志

--- src/main/java/com/akirahane/momentum/core/state/MovementStateMachine.java
+++ src/main/java/com/akirahane/momentum/core/state/MovementStateMachine.java
@@ -65,7 +65,7 @@ public class MovementStateMachine {
     // 状态转换
     private void transition(BaseState next, Player player) {
         if (next.equals(currentState)) return;
-        LOGGER.debug("[MovementStateMachine] {} to {}", currentState.getStateType(), next.getStateType());
+        LOGGER.trace("[MovementStateMachine] {} to {}", currentState.getStateType(), next.getStateType());
         currentState.onExit(player, context);
         currentState = next;
         currentState.onEnter(player, context);
